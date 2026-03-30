5e11c652239fde996eaedbcf03b4dce52d293f47
DoedeLimokus <doede@buchung.nl>
2026-03-30 18:29:36 +0300

    Added: Tick if-instance AND currentState

--- src/Board.ts
+++ src/Board.ts
@@ -3,31 +3,41 @@ export class Board {
   height;
   state: number;
   entity: string;
+  tickState: number;
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
     this.state = 0;
     this.entity = "";
+    this.tickState = 1;
   }
 
   toString() {
     let emptyRowArray = new Array(this.width).fill('.')
     let emptyRow = emptyRowArray.join('') + "\n"
+    let currentField
     if (this.state == 0) {
       let initial = emptyRow.repeat(this.height);
-      return initial;
-    } else if (this.state == 1) {
+      currentField = initial;
+    } else if (this.state == 1 && this.tickState == 0) {
       let entityRowArray = [...emptyRowArray]
       entityRowArray[Math.floor(entityRowArray.length / 2)] = this.entity
       let entityRow = entityRowArray.join('') + "\n";
-      return entityRow + emptyRow.repeat(this.height - 1);
+      currentField = entityRow + emptyRow.repeat(this.height - 1);
+    } else if (this.tickState == 1){
+      this.tickState = 0
     }
+    return currentField
   }
 
   drop(entity: string) {
     this.state = 1;
     this.entity = entity;
+  }
+
+  tick(){
+    this.tickState = 1;
   }
 }
 
