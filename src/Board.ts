562b37e99e8917d34f1bddb0e725c1014f0df631
DoedeLimokus <doede@buchung.nl>
2026-03-30 18:11:12 +0300

    Editted: Now Using arrays to define a row

--- src/Board.ts
+++ src/Board.ts
@@ -2,29 +2,32 @@ export class Board {
   width;
   height;
   state: number;
-  shape: string;
+  entity: string;
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
     this.state = 0;
-    this.shape = "";
+    this.entity = "";
   }
 
   toString() {
+    let emptyRowArray = new Array(this.width).fill('.')
+    let emptyRow = emptyRowArray.join('')
     if (this.state == 0) {
-      let initial = (".".repeat(this.width) + "\n").repeat(this.height);
+      let initial = (emptyRow + "\n").repeat(this.height);
       return initial;
     } else if (this.state == 1) {
-      let emptyRow = ".".repeat(this.width) + "\n";
-      let entityRow = `.${this.shape}.` + "\n";
+      let entityRowArray = emptyRowArray
+      entityRowArray[this.width / 2] = this.entity
+      let entityRow = `.${this.entity}.` + "\n";
       return entityRow + emptyRow.repeat(this.height - 1);
     }
   }
 
   drop(entity: string) {
     this.state = 1;
-    this.shape = entity;
+    this.entity = entity;
   }
 }
 
