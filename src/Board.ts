cc86b88cfe0d335fa42a139a092c3f48c73e730e
DoedeLimokus <doede@buchung.nl>
2026-03-30 20:14:14 +0300

    Completed: Falling Blocks

--- src/Board.ts
+++ src/Board.ts
@@ -7,6 +7,7 @@ export class Board {
   tickState: number;
   emptyRowArray: Array<string>
   currentField: Array<Array<string>>
+  dropStatus: number;
 
   constructor(width: number, height: number) {
     this.width = width;
@@ -14,29 +15,33 @@ export class Board {
     this.state = 0;
     this.entity = "";
     this.entityPosition = 0;
-    this.tickState = 1;
-
+    this.tickState = 0;
     this.currentField = Array()
+    this.dropStatus = 0
 
+    // console.log("init")
     this.emptyRowArray = new Array(this.width).fill('.')
     this.emptyRowArray.push('\n')
     let initial = this.emptyRowArray
     for (let i=1; i<= this.height; i++) {
     this.currentField.push(initial);
+
+    this.state = 1
     }
   }
 
   toString() {
-    if (this.state == 1) {
+    // console.log(this.state)
+    if (this.state == 1 && this.dropStatus == 1) {
+      // console.log("Drop")
+      this.dropStatus = 0;
       this.state = 2
       let entityRowArray = [...this.emptyRowArray]
       entityRowArray[Math.floor(this.width / 2)] = this.entity
-      this.currentField.push(entityRowArray);
-      for (let i=1; i<= (this.height - 1); i++) {
-        this.currentField.push(this.emptyRowArray);
-      }
+      this.currentField[0] = entityRowArray;
     }
-    if (this.tickState == 1 && this.state == 2){
+    if (this.state == 2 && this.tickState == 1){
+      // console.log("Tick")
       this.tickState = 0
       let pos1 = this.entityPosition
       let row1 = this.currentField[this.entityPosition]
@@ -50,10 +55,10 @@ export class Board {
   }
 
   drop(entity: string) {
-    if (this.state == 1){
+    if (this.state != 1){
       throw new Error("already falling")
     } else {
-    this.state = 1;
+    this.dropStatus = 1;
     this.entity = entity;
     }
   }
@@ -64,6 +69,8 @@ export class Board {
 }
 
 let board = new Board(3, 3);
+console.log(board.toString())
 board.drop("X")
+console.log(board.toString());
 board.tick()
 console.log(board.toString());
