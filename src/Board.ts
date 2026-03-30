73872a767bb0a24c217f754a26e76b5e7f2d2e03
DoedeLimokus <doede@buchung.nl>
2026-03-30 18:35:01 +0300

    Added: \n to end of array

--- src/Board.ts
+++ src/Board.ts
@@ -15,18 +15,20 @@ export class Board {
 
   toString() {
     let emptyRowArray = new Array(this.width).fill('.')
-    let emptyRow = emptyRowArray.join('') + "\n"
+    emptyRowArray.push('\n')
+    let emptyRow = emptyRowArray.join('')
     let currentField
     if (this.state == 0) {
       let initial = emptyRow.repeat(this.height);
       currentField = initial;
-    } else if (this.state == 1 && this.tickState == 0) {
+    } else if (this.state == 1) {
       let entityRowArray = [...emptyRowArray]
-      entityRowArray[Math.floor(entityRowArray.length / 2)] = this.entity
-      let entityRow = entityRowArray.join('') + "\n";
+      entityRowArray[Math.floor(this.width / 2)] = this.entity
+      let entityRow = entityRowArray.join('')
       currentField = entityRow + emptyRow.repeat(this.height - 1);
     } else if (this.tickState == 1){
       this.tickState = 0
+
     }
     return currentField
   }
