4062754bdd1260992482afbf1c2190b5197b6257
DoedeLimokus <doede@buchung.nl>
2026-03-30 19:09:04 +0300

    Completed: When a block is dropped

--- src/Board.ts
+++ src/Board.ts
@@ -3,6 +3,7 @@ export class Board {
   height;
   state: number;
   entity: string;
+  entityPosition: number;
   tickState: number;
 
   constructor(width: number, height: number) {
@@ -10,6 +11,7 @@ export class Board {
     this.height = height;
     this.state = 0;
     this.entity = "";
+    this.entityPosition = 0;
     this.tickState = 1;
   }
 
@@ -23,21 +25,34 @@ export class Board {
       currentField.push(initial);
       }
     } else if (this.state == 1) {
+      this.state = 2
       let entityRowArray = [...emptyRowArray]
       entityRowArray[Math.floor(this.width / 2)] = this.entity
       currentField.push(entityRowArray);
       for (let i=1; i<= (this.height - 1); i++) {
         currentField.push(emptyRowArray);
       }
-    } else if (this.tickState == 1){
-      this.tickState = 0
     }
+    if (this.tickState == 1 && this.state == 2){
+    this.tickState = 0
+    let pos1 = this.entityPosition
+    let row1 = currentField[this.entityPosition]
+    this.entityPosition++
+    let pos2 = this.entityPosition
+    let row2 = currentField[this.entityPosition]
+    currentField[pos1] = row2
+    currentField[pos2] = row1
+  }
     return (currentField.map(rij => rij.join(''))).join('')
   }
 
   drop(entity: string) {
+    if (this.state == 1){
+      throw new Error("already falling")
+    } else {
     this.state = 1;
     this.entity = entity;
+    }
   }
 
   tick(){
@@ -47,4 +62,5 @@ export class Board {
 
 let board = new Board(3, 3);
 board.drop("X")
+board.tick()
 console.log(board.toString());
--- test/FallingBlocks.test.mjs
+++ test/FallingBlocks.test.mjs
@@ -30,7 +30,7 @@ describe("Falling blocks", () => {
       );
     });
 
-    test.skip("it moves down one row per tick", () => {
+    test("it moves down one row per tick", () => {
       board.tick();
 
       expect(board.toString()).to.equalShape(
@@ -40,7 +40,7 @@ describe("Falling blocks", () => {
       );
     });
 
-    test.skip("at most one block may be falling at a time", () => {
+    test("at most one block may be falling at a time", () => {
       const before = board.toString();
       expect(() => board.drop("Y")).to.throw("already falling");
       const after = board.toString();
