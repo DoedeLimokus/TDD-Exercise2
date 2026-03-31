9628b9754e0c182971d97a540797c8baa2f46d7c
DoedeLimokus <doede@buchung.nl>
2026-03-31 16:34:40 +0300

    Completed Level 3

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -1,11 +1,62 @@
 import { RotatingShape } from "./RotatingShapes.ts";
 
 export class Tetromino {
-  currentField = Array();
+  private orientations: RotatingShape[];
+  private index: number;
 
-  static T_SHAPE = RotatingShape.fromString(`.T.
-        TTT
-        ...`);
+  constructor(orientations: RotatingShape[], index = 0) {
+    this.orientations = orientations;
+    this.index = index;
+  }
+
+  toString() {
+    return this.orientations[this.index].toString();
+  }
+
+  rotateRight() {
+    const nextIndex = (this.index + 1) % this.orientations.length;
+    return new Tetromino(this.orientations, nextIndex);
+  }
+
+  rotateLeft() {
+    const nextIndex = (this.index - 1 + this.orientations.length) % this.orientations.length;
+    return new Tetromino(this.orientations, nextIndex);
+  }
+
+  static T_SHAPE = new Tetromino([
+    RotatingShape.fromString(`.T.
+    TTT
+    ...`),
+    RotatingShape.fromString(`.T.
+    .TT
+    .T.`),
+    RotatingShape.fromString(`...
+    TTT
+    .T.`),
+    RotatingShape.fromString(`.T.
+    TT.
+    .T.`),
+  ]);
+
+  static I_SHAPE = new Tetromino([
+    RotatingShape.fromString(`.....
+        .....
+        IIII.
+        .....
+        .....`),
+
+    RotatingShape.fromString(`..I..
+    ..I..
+    ..I..
+    ..I..
+    .....`),
+  ]);
+
+  static O_SHAPE = new Tetromino([
+    RotatingShape.fromString(`.OO
+    .OO
+    ...`),
+  ]);
 }
 
 const Tetris = Tetromino.T_SHAPE;
