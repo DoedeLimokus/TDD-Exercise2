5246ed8c6904126f814cbf7bad6cc7cc4defcf5f
DoedeLimokus <doede@buchung.nl>
2026-03-31 15:35:08 +0300

    Completed Level 2

---
+++ src/test.ts
@@ -0,0 +1,20 @@
+export class Auto{
+    bandSoort = ''
+
+    constructor(){
+    }
+
+    static band(soort:string){
+        const BMW = new Auto
+        BMW.bandSoort = soort
+        return BMW
+    }
+
+    printBand(){
+        return this.bandSoort
+    }
+
+}
+
+const NieuweBMW = Auto.band("Winter")
+console.log(NieuweBMW.printBand())
